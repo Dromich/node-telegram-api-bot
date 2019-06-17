bd01678daeeeab12dd7b666b1ce1871804acadfb
dromich <hrushch.rr57@gmail.com>
2019-06-17 19:00:47 +0300

    Тест клавіатури 1

--- main.js
+++ main.js
@@ -14,8 +14,61 @@ const bot = new TelegramBot(TOKEN.T,{
 	}
 	
 })
-bot.on('message',msg =>{
-	
+bot.on('message',msg =>{	
 	const { id } = msg.chat 
-	bot.sendMessage(id,helps.debug(msg))
+
+	// const html = `
+	// <strong>Helo ${msg.from.username}</strong>
+	
+	// <a href="google.com">google</a>
+	// `
+
+	// if (msg.text.toLowerCase() == 'hello') {
+	// 	bot.sendMessage(id,`Heloo ${msg.from.username}`)
+	// }else{
+	// 	bot.sendMessage(id,html,{
+	// 		parse_mode:'HTML'
+	// 	})
+	// }
+	
+switch (msg.text ) {
+	case 'Закрити':
+		bot.sendMessage(id,'Закриваю клавіатуру',{
+			reply_markup:{
+				remove_keyboard:true
+			}
+		})
+		break;
+		case 'Місце':
+		bot.sendMessage(id,'Закриваю клавіатуру',{
+			reply_markup:{
+				force_reply:true
+			}
+		})
+		break;
+	default:
+	console.log('deffff')
+		break;
+}
+
+bot.sendMessage(id,'Клавіатура',{
+	reply_markup:{
+		keyboard:[
+			['Місце'],
+			['Відповідь','Закрити'],
+			['Відправити контакт']
+
+		]
+	}
+
 })
+
+
+})
+
+// bot.onText(/\/start/,msg =>{
+// 	const { id } = msg.chat
+
+// 	bot.sendMessage(id,helps.debug(msg))
+
+// })
